d924a9129c9269dc8e2f6863ecf97acfd40baa84
julien54190 <lucadello.julien@hotmail.fr>
2025-04-02 17:41:07 +0200

    add Setting up the list of questions

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,8 +11,42 @@ final class HomeController extends AbstractController
     #[Route('/', name: 'home')]
     public function index(): Response
     {
+        $question = [
+            [
+                'title' => 'How to become a web developer?',
+                'content' => 'I want to become a web developer. What should I do first?',
+                'rating' => 20,
+                'author' => [
+                    'name' => 'John Doe',
+                    'avatar' => 'https://randomuser.me/api/portraits/men/36.jpg',
+                ],
+                'nbrOfReponses' => 15
+                
+            ],
+            [
+                'title' => 'How to become a mobile developer?',
+                'content' => 'I want to become a mobile developer. What should I do first?',
+                'rating' => 0,
+                'author' => [
+                    'name' => 'Jane Doe',
+                    'avatar' => 'https://randomuser.me/api/portraits/women/33.jpg',
+                ],
+                'nbrOfReponses' => 10
+            ],
+            [
+                'title' => 'How to become a data scientist?',
+                'content' => 'I want to become a data scientist. What should I do first?',
+                'rating' => -10,
+                'author' => [
+                    'name' => 'Alice Doe',
+                    'avatar' => 'https://randomuser.me/api/portraits/women/23.jpg',
+                ],
+                'nbrOfReponses' => 5
+            ],
+        ];
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'questions' => $question,
         ]);
     }
 }
