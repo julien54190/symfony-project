81659515d34532ad8e0a1344e4bf966a2e63db45
julien54190 <lucadello.julien@hotmail.fr>
2025-04-04 11:25:51 +0200

    add Display of votes

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,56 +2,19 @@
 
 namespace App\Controller;
 
+use App\Repository\QuestionRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
-final class HomeController extends AbstractController
+class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function index(): Response
+    public function index(QuestionRepository $questionRepository): Response
     {
-
-        
-        $question = [
-            [
-                'id' => '1',
-                'title' => 'How to become a web developer?',
-                'content' => 'I want to become a web developer. What should I do first?',
-                'rating' => 20,
-                'author' => [
-                    'name' => 'John Doe',
-                    'avatar' => 'https://randomuser.me/api/portraits/men/36.jpg',
-                ],
-                'nbrOfReponses' => 15
-                
-            ],
-            [
-                'id' => '2',
-                'title' => 'How to become a mobile developer?',
-                'content' => 'I want to become a mobile developer. What should I do first?',
-                'rating' => 0,
-                'author' => [
-                    'name' => 'Jane Doe',
-                    'avatar' => 'https://randomuser.me/api/portraits/women/33.jpg',
-                ],
-                'nbrOfReponses' => 10
-            ],
-            [
-                'id' => '3',
-                'title' => 'How to become a data scientist?',
-                'content' => 'I want to become a data scientist. What should I do first?',
-                'rating' => -10,
-                'author' => [
-                    'name' => 'Alice Doe',
-                    'avatar' => 'https://randomuser.me/api/portraits/women/23.jpg',
-                ],
-                'nbrOfReponses' => 5
-            ],
-        ];
-
+        $questions = $questionRepository->findAll();
         return $this->render('home/index.html.twig', [
-            'questions' => $question,
+            'questions' => $questions
         ]);
     }
 }
--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -46,6 +46,7 @@ final class QuestionController extends AbstractController
             $comment->setCreatedAt(new \DateTimeImmutable());
             $comment->setRating(0);
             $comment->setQuestion($question);
+            $question->setNbrOfResponse($question->getNbrOfResponse() + 1);
             $em->persist($comment);
             $em->flush();
             $this->addFlash('success', 'Votre réponse a bien été ajouté!');
