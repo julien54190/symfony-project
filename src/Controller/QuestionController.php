f2a40962b537954c88bde2d7d8c741aa0189e4d2
julien54190 <lucadello.julien@hotmail.fr>
2025-04-04 12:07:03 +0200

    add Implementation of Voting

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -9,14 +9,15 @@ use App\Form\QuestionType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-final class QuestionController extends AbstractController
+class QuestionController extends AbstractController
 {
     #[Route('/question/ask', name: 'question_form')]
     public function index(Request $request, EntityManagerInterface $em): Response
     {
+
         $question = new Question();
         $formQuestion = $this->createForm(QuestionType::class, $question);
 
@@ -28,7 +29,7 @@ final class QuestionController extends AbstractController
             $question->setCreatedAt(new \DateTimeImmutable());
             $em->persist($question);
             $em->flush();
-            $this->addFlash('success', 'Votre question a bien été ajoutée !');
+            $this->addFlash('success', 'Votre question a été ajoutée');
             return $this->redirectToRoute('home');
         }
 
@@ -36,9 +37,10 @@ final class QuestionController extends AbstractController
             'form' => $formQuestion->createView(),
         ]);
     }
-    #[Route('/question/{id}', name: 'question_show')]
-    public function show(Request $request, Question $question, EntityManagerInterface $em): Response {
 
+    #[Route('/question/{id}', name: 'question_show')]
+    public function show(Request $request, Question $question, EntityManagerInterface $em): Response
+    {
         $comment = new Comment();
         $commentForm = $this->createForm(CommentType::class, $comment);
         $commentForm->handleRequest($request);
@@ -49,18 +51,31 @@ final class QuestionController extends AbstractController
             $question->setNbrOfResponse($question->getNbrOfResponse() + 1);
             $em->persist($comment);
             $em->flush();
-            $this->addFlash('success', 'Votre réponse a bien été ajouté!');
+            $this->addFlash('success', 'Votre réponse a bien été ajoutée');
             return $this->redirect($request->getUri());
         }
 
-
-            // 'name' => 'John Doe',
-            // 'avatar' => 'https://randomuser.me/api/portraits/men/36.jpg',
-
-
         return $this->render('question/show.html.twig', [
             'question' => $question,
-            'form' => $commentForm->createView(),
+            'form' => $commentForm->createView()
         ]);
+    }
+
+    #[Route('/question/rating/{id}/{score}', name: 'question_rating')]
+    public function ratingQuestion(Request $request, Question $question, int $score, EntityManagerInterface $em)
+    {
+        $question->setRating($question->getRating() + $score);
+        $em->flush();
+        $referer = $request->server->get('HTTP_REFERER');
+        return $referer ? $this->redirect($referer) : $this->redirectToRoute('home');
+    }
+
+    #[Route('/comment/rating/{id}/{score}', name: 'comment_rating')]
+    public function ratingComment(Request $request, Comment $comment, int $score, EntityManagerInterface $em)
+    {
+        $comment->setRating($comment->getRating() + $score);
+        $em->flush();
+        $referer = $request->server->get('HTTP_REFERER');
+        return $referer ? $this->redirect($referer) : $this->redirectToRoute('home');
     }
 }
