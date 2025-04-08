0d5153ee7c9fd4e8dc12009c9c90ca676c8a7d02
julien54190 <lucadello.julien@hotmail.fr>
2025-04-08 08:49:34 +0200

    add Road and topbar protection

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -11,13 +11,15 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class QuestionController extends AbstractController
 {
     #[Route('/question/ask', name: 'question_form')]
+    #[IsGranted('ROLE_USER')]
     public function index(Request $request, EntityManagerInterface $em): Response
     {
-
+        $user = $this->getUser();
         $question = new Question();
         $formQuestion = $this->createForm(QuestionType::class, $question);
 
@@ -27,6 +29,7 @@ class QuestionController extends AbstractController
             $question->setNbrOfResponse(0);
             $question->setRating(0);
             $question->setCreatedAt(new \DateTimeImmutable());
+            $question->setAuthor($user);
             $em->persist($question);
             $em->flush();
             $this->addFlash('success', 'Votre question a été ajoutée');
@@ -41,27 +44,35 @@ class QuestionController extends AbstractController
     #[Route('/question/{id}', name: 'question_show')]
     public function show(Request $request, Question $question, EntityManagerInterface $em): Response
     {
-        $comment = new Comment();
-        $commentForm = $this->createForm(CommentType::class, $comment);
-        $commentForm->handleRequest($request);
-        if ($commentForm->isSubmitted() && $commentForm->isValid()) {
-            $comment->setCreatedAt(new \DateTimeImmutable());
-            $comment->setRating(0);
-            $comment->setQuestion($question);
-            $question->setNbrOfResponse($question->getNbrOfResponse() + 1);
-            $em->persist($comment);
-            $em->flush();
-            $this->addFlash('success', 'Votre réponse a bien été ajoutée');
-            return $this->redirect($request->getUri());
+        $options = [
+            'question' => $question
+        ];
+        $user = $this->getUser();
+        if ($user){
+            $comment = new Comment();
+            $commentForm = $this->createForm(CommentType::class, $comment);
+            $commentForm->handleRequest($request);
+            if ($commentForm->isSubmitted() && $commentForm->isValid()) {
+                $comment->setCreatedAt(new \DateTimeImmutable());
+                $comment->setRating(0);
+                $comment->setQuestion($question);
+                $comment->setAuthor($user);
+                $question->setNbrOfResponse($question->getNbrOfResponse() + 1);
+                $em->persist($comment);
+                $em->flush();
+                $this->addFlash('success', 'Votre réponse a bien été ajoutée');
+                return $this->redirect($request->getUri());
+            }
+            $options['form'] = $commentForm->createView();
         }
 
-        return $this->render('question/show.html.twig', [
-            'question' => $question,
-            'form' => $commentForm->createView()
-        ]);
+
+
+        return $this->render('question/show.html.twig', $options);
     }
 
     #[Route('/question/rating/{id}/{score}', name: 'question_rating')]
+    #[IsGranted('ROLE_USER')]
     public function ratingQuestion(Request $request, Question $question, int $score, EntityManagerInterface $em)
     {
         $question->setRating($question->getRating() + $score);
@@ -71,6 +82,7 @@ class QuestionController extends AbstractController
     }
 
     #[Route('/comment/rating/{id}/{score}', name: 'comment_rating')]
+    #[IsGranted('ROLE_USER')]
     public function ratingComment(Request $request, Comment $comment, int $score, EntityManagerInterface $em)
     {
         $comment->setRating($comment->getRating() + $score);
